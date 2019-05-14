661a65987b92049b527d67ba940be9eba576564f
omardcan <38349951+omardcan@users.noreply.github.com>
2019-05-14 15:13:59 -0300

    Classe Jogador, Classe Fundo Criadas

--- Base.py
+++ Base.py
@@ -6,12 +6,31 @@ import sys, pygame
 jogando = 1
 pygame.init()
 definicao = width, height = 1000, 800
-tabuleiro = pygame.image.load("tabuleiro.png")
 tela = pygame.display.set_mode(definicao)
+
+class Jogador(pygame.sprite.Sprite):
+
+    def __init__(self,foto_peao,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(foto_peao)
+        self.image = pygame.transform.scale(self.image,(170,100))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Fundo(pygame.sprite.Sprite):
+
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("tabuleiro.jpg").convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 
 while jogando:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit() 
-	tela.blit(tabuleiro, (0,0))
+	pygame.display.update()
 	pygame.display.flip()
