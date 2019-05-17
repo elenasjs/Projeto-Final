d12848462f9a04e94c3c2ee305c7f71c41f27ec7
omardcan <38349951+omardcan@users.noreply.github.com>
2019-05-17 08:04:00 -0300

    dado sorteando, player 1 andando e fundo func

--- Base.py
+++ Base.py
@@ -2,11 +2,23 @@ import pygame
 import pygame.locals
 from random import randrange
 import sys, pygame
+import numpy as np
 
 jogando = 1
 pygame.init()
 definicao = width, height = 1000, 800
 tela = pygame.display.set_mode(definicao)
+relogio =pygame.time.Clock()
+FPS = 40
+arial = pygame.font.match_font('arial')
+vermelho = (230,0,0)
+
+def Escreve(texto,size,fonte,cor,x,y):
+    font = pygame.font.Font(fonte,size)
+    ts = font.render(texto,True,cor)
+    texto_rect = ts.get_rect()
+    texto_rect.midtop = (x,y)
+    tela.blit(ts,texto_rect)
 
 class Jogador(pygame.sprite.Sprite):
 
@@ -18,19 +30,61 @@ class Jogador(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+    def move(self,dado):
+    	if self.rect.y <=10:
+    		self.rect.x += dado*10
+    	if self.rect.x >= 300:
+    		self.rect.y += dado*10
+    	if self.rect.x > 10 and self.rect.y >= 300:
+    		self.rect.x -= dado*10
+    	if self.rect.x <= 10 and self.rect.y > 10:
+    		self.rect.y -= dado*10
+
+
 class Fundo(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load("tabuleiro.png").convert()
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
 
-    def __init__(self,x,y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("tabuleiro.jpg").convert()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+
+player_group = pygame.sprite.Group()
+fundo_group = pygame.sprite.Group()
+
+player_1 = Jogador("imagem_peao.png",5,5)
+fundo = Fundo(0,0)
+
+player_group.add(player_1)
+fundo_group.add(fundo)
 
 
+dado = "-1"
+
+jogando = True
 while jogando:
+	relogio.tick(FPS)
+	fundo_group.draw(tela)
 
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit() 
+		if event.type == pygame.QUIT:
+			pygame.quit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+			if event.key == pygame.K_SPACE:
+				dado = np.random.randint(1,7)
+				player_1.move(dado)
+
+
+	
+	Escreve("Dado Sorteado: {}".format(dado),28,arial,vermelho,200,160)
+	player_group.draw(tela)
+
 	pygame.display.update()
 	pygame.display.flip()
+
+
+
+
